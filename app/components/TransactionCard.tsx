2b92cb1a968fd568c98c2dc7660141649bb6af1d
Finn <46631279+CodeShady@users.noreply.github.com>
2025-07-04 21:42:13 -0400

    feat(app): UI improvements

--- app/components/TransactionCard.tsx
+++ app/components/TransactionCard.tsx
@@ -1,14 +1,20 @@
 import { Transaction } from "@/types";
 import HashDisplay from "./ui/HashDisplay";
 import TimeAgo from "./ui/TimeAgo";
-import { ArrowRight, Check, Clock } from "lucide-react";
+import { ArrowDown, ArrowRight, ArrowUp, Check, Clock } from "lucide-react";
 import { cn } from "@/lib/utils";
 
-const TransactionCard = ({ transaction, pending, className="" }: { transaction: Transaction; pending: boolean; className?: string }) => {
+const TransactionCard = ({ transaction, pending, transactionDirection, className="" }: { transaction: Transaction; pending: boolean; transactionDirection?: "inbound" | "outbound"; className?: string }) => {
   return (
     <div className={cn("w-full bg-white/15 rounded-xl p-4 border flex flex-col gap-2", className)}>
       <div className="flex items-center justify-between">
         <span className="text-xl font-bold flex items-center gap-1">
+          {transactionDirection && (
+            <span className="text-lg text-muted-foreground font-medium">
+              {transactionDirection === "outbound" && <ArrowUp size={16} />}
+              {transactionDirection === "inbound" && <ArrowDown size={16} />}
+            </span>
+          )}
           <span>{transaction.amount}</span>
           <span className="text-sm text-muted-foreground font-medium">BOND</span>
         </span>
@@ -32,8 +38,8 @@ const TransactionCard = ({ transaction, pending, className="" }: { transaction: 
         </span>
       </div>
       {transaction.message && (
-        <div className="mt-1 text-sm text-muted-foreground italic">
-          &ldquo;{transaction.message}&rdquo;
+        <div className="p mt-1 md:text-sm text-muted-foreground">
+          {transaction.message}
         </div>
       )}
     </div>
--- app/components/user/UserTransactions.tsx
+++ app/components/user/UserTransactions.tsx
@@ -1,12 +1,10 @@
 "use client";
 
 import { fetchConfirmedTransactions, fetchPendingTransactions } from "@/lib/api/fetchTransactions";
-import { ArrowDown, ArrowRight, ArrowUp, Check, Clock } from "lucide-react";
 import { useEffect, useState } from "react";
-import HashDisplay from "../ui/HashDisplay";
-import TimeAgo from "../ui/TimeAgo";
 import { useWallet } from "@/lib/hooks/useWallet";
 import { Transaction } from "@/types";
+import TransactionCard from "../TransactionCard";
 
 const UserTransactions = () => {
   const { address } = useWallet();
@@ -29,51 +27,10 @@ const UserTransactions = () => {
   }, [address]);
 
   return (
-    <div className="overflow-y-scroll">
+    <div className="overflow-y-scroll space-y-2">
       {loading && <p className="text-muted-foreground">Loading...</p>}
-      {pending.map((tx, index) => <TransactionCard key={index} pending={true} tx={tx} />)}
-      {confirmed.map((tx, index) => <TransactionCard key={index} pending={false} tx={tx} />)}
-    </div>
-  );
-};
-
-const TransactionCard = ({ pending, tx }: { pending: boolean; tx: Transaction }) => {
-  const { address } = useWallet();
-
-  if (!address) return ;
-
-  return (
-    <div className="w-full bg-white/15 rounded-xl p-4 mb-2 shadow-sm border flex flex-col gap-2">
-      <div className="flex items-center justify-between">
-        <span className="text-xl font-bold flex items-center gap-1">
-          <span className="text-lg text-muted-foreground font-medium">{tx.sender === address ? <ArrowUp size={16} /> : <ArrowDown size={16} />}</span>
-          <span>{tx.amount}</span>
-          <span className="text-sm text-muted-foreground font-medium">BOND</span>
-        </span>
-        <span className="text-sm text-muted-foreground"><TimeAgo iso={tx.timestamp} /></span>
-      </div>
-      <div className="flex items-center justify-between">
-        <div className="flex items-center gap-2">
-          <HashDisplay hash={tx.sender} />
-          <ArrowRight size={16} className="text-muted-foreground" />
-          <HashDisplay hash={tx.recipient} />
-        </div>
-        
-        <span
-          className={`text-sm px-2 py-1.5 rounded-full font-semibold ${
-            pending
-              ? "bg-transparent border text-warning"
-              : "bg-transparent border text-success"
-          }`}
-        >
-          {pending ? <Clock size={16} /> : <Check size={16} />}
-        </span>
-      </div>
-      {tx.message && (
-        <div className="mt-1 text-sm text-muted-foreground italic truncate">
-          &ldquo;{tx.message}&rdquo;
-        </div>
-      )}
+      {pending.map((tx, index) => <TransactionCard key={index} pending={true} transaction={tx} transactionDirection={tx.sender === address ? "inbound" : "outbound"} />)}
+      {confirmed.map((tx, index) => <TransactionCard key={index} pending={false} transaction={tx} transactionDirection={tx.recipient === address ? "inbound" : "outbound"} />)}
     </div>
   );
 };
